b8b22159a18ce41babe7a8033e6f7041bbd8d6c9
Arsenkats <kats45mam@gmail.com>
2024-07-02 16:53:53 +0300

    task 5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const formElem = document.querySelector('.login-form');
+formElem.addEventListener('submit', event => {
+      event.preventDefault();
 
+            const form = new FormData(formElem);
+            const data = {};
+            let isFormValid = true;
+
+            form.forEach((value, key) => {
+                const trimmedValue = value.trim();
+                if (trimmedValue === "") {
+                    isFormValid = false;
+                    alert('All fields must be filled out.');
+                    return;
+                }
+                data[key] = trimmedValue;
+            });
+
+            if (isFormValid) {
+                console.log(data);
+                formElem.reset(); 
+            }
+});
